629615bf583ee7a6ced903cec3134c7402b9c5df
MaksymKalennyk <107300165+MaksymKalennyk@users.noreply.github.com>
2023-09-06 19:32:22 +0300

    Adding first part of front

--- Front/src/app/app.module.ts
+++ Front/src/app/app.module.ts
@@ -2,14 +2,33 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CinemaComponent } from './cinema/cinema.component';
+import {RouterModule, Routes} from "@angular/router";
+import { SessionComponent } from './cinema/session/session.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: SessionComponent,
+    children: [
+      { path: 'session', component: SessionComponent },
+    ],
+  },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CinemaComponent,
+    SessionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
